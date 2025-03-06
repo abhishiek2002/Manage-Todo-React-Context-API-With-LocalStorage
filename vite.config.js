1347b4051f3bc3297c262007f3bfd15cec1121b6
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-06 17:21:32 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  // base for telling that ./ is our home page
   plugins: [react(), tailwindcss()],
 });
